15eb1036f853317a8d7bd72b75607765de1e3192
Duarte <guiduart3223@gmail.com>
2024-03-01 09:11:32 -0300

    criado array de e-mail

---
+++ src/utils/emails.ts
@@ -0,0 +1,88 @@
+export type EmailDataProps = typeof EMAILS[0]
+
+export const EMAILS = [
+    {
+        id: "1",
+        name: "Carlos Eduardo",
+        avatar: "https://mighty.tools/mockmind-api/content/human/42.jpg", 
+        marker: "Importante",
+        subject: "O que é o React Native?",
+        message: "React Native é uma biblioteca nativa JavaScript criada pelo Facebook. É usada para desenvolver aplicativos para os sistemas Android e IOS  de forma nativa. ",
+        star: false,
+        date: "01 de mar."
+    },
+    {
+        id: "2",
+        name: "José Freitas",
+        avatar: "https://mighty.tools/mockmind-api/content/human/5.jpg", 
+        subject: "Recursos e Funcionalidades do React Native",
+        message: "Reutilização de Código, Performance, Biblioteca de Componentes Pré-Construídos, Eficiência de Desenvolvimento, Acesso às APIs Nativas",
+        star: true,
+        date: "01 de mar."
+    },
+    {
+        id: "3",
+        name: "Marciene Lipa",
+        avatar: "https://mighty.tools/mockmind-api/content/human/8.jpg", 
+        subject: "O que é Lorem Ipsum?",
+        message: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI",
+        star: false,
+        date: "01 de mar."
+    },
+    {
+        id: "4",
+        name: "Carlos Eduardo",
+        avatar: "https://mighty.tools/mockmind-api/content/human/42.jpg", 
+        marker: "Importante",
+        subject: "O que é o React Native?",
+        message: "React Native é uma biblioteca nativa JavaScript criada pelo Facebook. É usada para desenvolver aplicativos para os sistemas Android e IOS  de forma nativa. ",
+        star: false,
+        date: "01 de mar."
+    },
+    {
+        id: "5",
+        name: "José Freitas",
+        avatar: "https://mighty.tools/mockmind-api/content/human/5.jpg", 
+        subject: "Recursos e Funcionalidades do React Native",
+        message: "Reutilização de Código, Performance, Biblioteca de Componentes Pré-Construídos, Eficiência de Desenvolvimento, Acesso às APIs Nativas",
+        star: true,
+        date: "01 de mar."
+    },
+    {
+        id: "6",
+        name: "Marciene Lipa",
+        avatar: "https://mighty.tools/mockmind-api/content/human/8.jpg", 
+        subject: "O que é Lorem Ipsum?",
+        message: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI",
+        star: false,
+        date: "01 de mar."
+    },
+    {
+        id: "7",
+        name: "Carlos Eduardo",
+        avatar: "https://mighty.tools/mockmind-api/content/human/42.jpg", 
+        marker: "Importante",
+        subject: "O que é o React Native?",
+        message: "React Native é uma biblioteca nativa JavaScript criada pelo Facebook. É usada para desenvolver aplicativos para os sistemas Android e IOS  de forma nativa. ",
+        star: false,
+        date: "01 de mar."
+    },
+    {
+        id: "8",
+        name: "José Freitas",
+        avatar: "https://mighty.tools/mockmind-api/content/human/5.jpg", 
+        subject: "Recursos e Funcionalidades do React Native",
+        message: "Reutilização de Código, Performance, Biblioteca de Componentes Pré-Construídos, Eficiência de Desenvolvimento, Acesso às APIs Nativas",
+        star: true,
+        date: "01 de mar."
+    },
+    {
+        id: "9",
+        name: "Marciene Lipa",
+        avatar: "https://mighty.tools/mockmind-api/content/human/8.jpg", 
+        subject: "O que é Lorem Ipsum?",
+        message: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI",
+        star: false,
+        date: "01 de mar."
+    },
+]
